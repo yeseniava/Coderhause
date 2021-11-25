6fb63819ef6710e6be2eb360339f4c3716c540c4
Yesenia Valencia <yesenia@highstreet.com.mx>
2021-11-24 20:50:13 -0600

    quitar condición innecesaria

--- Desafios/02/js/desafio02.js
+++ Desafios/02/js/desafio02.js
@@ -3,16 +3,15 @@ const numRandom = Math.floor(Math.random()*11);
 console.log (numRandom);
 let numero = obtenerNumero();
 
-    while(true) { 
-        if(numero == numRandom){
-            alert("adivinaste el numero felicidades");
-            break;
-        } else if(numero < numRandom){
+    while(numero != numRandom) { 
+        if(numero < numRandom){
             numero = obtenerNumero("Error intenta con un numero mayor del 1 al 10");
         }else{
             numero = obtenerNumero("Error intenta con un numero menor del 1 al 10");
         }
     }
+        alert("adivinaste el numero felicidades");
+
 
 
 function obtenerNumero(mensaje = "Adivina un numero del 1 al 10"){
