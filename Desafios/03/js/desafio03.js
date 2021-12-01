5f35de39ffbc93253b1e3d58d9cb7dfd52886916
Yesenia Valencia <yesenia@highstreet.com.mx>
2021-12-01 16:34:17 -0600

    tablas de multiplicar 1 a n numero

---
+++ Desafios/03/js/desafio03.js
@@ -0,0 +1,11 @@
+let tabla = parseInt(prompt("Tablas de multiplar del 1 al n "));
+
+for(let k = 1; k <= tabla; k++){
+    for(let i = 1; i <= 10; i++){
+        // console.log("la tabla de multiplicar de " + i +" por " +  " es: ");
+            console.log( k +" x " + i +  " = " + i*k);
+    }
+}
+
+
+ 
