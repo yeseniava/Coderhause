9b130cc4460479702959142ef8bc9a2e432b0350
Yesenia Valencia <yesenia@highstreet.com.mx>
2021-12-15 22:05:03 -0600

    update

--- Desafios/08/js/desafio08.js
+++ Desafios/08/js/desafio08.js
@@ -1,25 +1,107 @@
 
-function ingresarNombres(nombres){
-    let nombre = [] ;
-    while(input = prompt("ingresa solo nombres de mascotas")){
-        nombre.push(input); 
+/*
+Proyecto ecommerce apparel
+Guardar productos en arreglos 
+nombre
+sku
+descripcion
+costo
 
+costo sugerir venta al publico 
+sugerir precio al publico teniendo ganancias de un 200%
+optativo a cambiar 
+
+Modulo de descuentos
+ingresar sku del producto y aplicar decuento guardar e impromor ficha tecnica del producto
+
+*/
+class Product {
+
+    constructor( name, price) {
+        this.name = name;
+        this.price = price ? price : this.suggestedPrice()
     }
-    return nombre;
+
+    suggestedPrice(){
+        return this.cost * 3;
+    }
+
+    getPrice() {
+        return this.price - (this.price * (this.discount/100))
+    }
+
+    printProductDetails() {
+        console.log( "Nombre: " + this.name.toUpperCase() +  "\n" + "Precio Sugerido: "+ this.price );
+    }
+
 }
 
-function letras(nombres){
-    const lengths = nombres.map(nombre => nombre.length);
-    return lengths;
+let myProducts = [];
+showMenu();
+
+
+function showMenu(){
+    let option = prompt("Elige la opcion deseada \n1.- Agregar productos\n2.-Mostrar Productos\n3.-Mostrar ordenados por nombre \n4.-Mostrar ordenados por precio");
+    option = parseInt(option);
+    switch(option){
+        case 1:
+            addProduct();
+            break;
+        case 2:
+            printProducts();         
+            break;
+        case 3:
+            orderByName();         
+            break;
+        case 4:
+            orderByPrice();         
+            break;
+        // TO-DO: Add more menu options   
+    }
 }
 
-function ordenar(nombres){
-    ordn = nombres.sort();
-    return ordn;
-   }
-mascotas = ingresarNombres();
-console.log("mis mascotas " +mascotas);
-letrasArray = letras(mascotas);
-console.log("mis mascotas letras " +letrasArray);
-ord = ordenar(mascotas);
-console.log("mis mascotas ordenadas" +ord);
+function addProduct(){
+    let temProd = new Product(prompt("Nombre del producto"), getNumber(), 0);
+        myProducts.push(temProd);
+        showMenu();
+
+}
+
+function getNumber(msg = "Costo del producto solo numeros"){
+    let num;
+    while(isNaN(num)){
+        num = parseInt( prompt(msg));
+     }
+     return num;
+}
+
+function printProducts(){
+    myProducts.forEach(producto => {
+        producto.printProductDetails();
+    });
+}
+
+
+function orderByName(){
+
+    myProducts.sort((a, b) =>{
+        if ( a.name < b.name ){
+            return -1;
+          }
+          if ( a.name > b.name ){
+            return 1;
+          }
+          return 0;
+    });
+    printProducts();
+ 
+}
+
+function orderByPrice(){
+
+    myProducts.sort( (next, prev) => {
+        return prev.price - next.price;
+    });
+    printProducts();
+ 
+}
